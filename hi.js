e44405ea5cd14875f3da2032ad9253edd10f0c95
huyenlamchiton <37010776+huyenlamchiton@users.noreply.github.com>
2018-05-22 15:31:24 +0700

    Update hi.js

--- hi.js
+++ hi.js
@@ -1,4 +1,13 @@
-﻿function openNav() {
+var d=new Date();
+if(d.getDay==0){
+	document.getElementById("cn").className="hii"}
+else if(d.getDay()==1){document.getElementById('t2').className="hii";}
+else if(d.getDay()==2){document.getElementById('t3').className="hii";}
+else if(d.getDay()==3){document.getElementById("t4").className="hii";}
+else if(d.getDay()==4){document.getElementById("t5").className="hii";}
+else if(d.getDay()==5){document.getElementById("t6").className="hii";}
+else if(d.getDay()==6){document.getElementById("t7").className="hii";}
+function openNav() {
     document.getElementById("sideNavigation").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
 }
@@ -6,4 +15,12 @@
 function closeNav() {
     document.getElementById("sideNavigation").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+}
+function b64(){
+	var x=document.getElementById("b64").value;
+	document.getElementById("b64").value=btoa(x);
+}
+function d64(){
+	var x=document.getElementById("b64").value;
+	document.getElementById("b64").value=atob(x);
 }
