b47b96d6ea629ee3b493a0e5970679ae644b6eb5
huyenlamchiton <37010776+huyenlamchiton@users.noreply.github.com>
2018-05-22 16:07:04 +0700

    Update hi.js

--- hi.js
+++ hi.js
@@ -1,3 +1,8 @@
+
+function openNav() {
+    document.getElementById("sideNavigation").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
  
 function closeNav() {
     document.getElementById("sideNavigation").style.width = "0";
@@ -10,4 +15,15 @@ function b64(){
 function d64(){
 	var x=document.getElementById("b64").value;
 	document.getElementById("b64").value=atob(x);
+}
+function xacdinh(){
+	var d=new Date();
+    if(d.getDay==0){
+    document.getElementById("cn").className="hii"}	
+	else if(d.getDay()==1){document.getElementById('t2').className="hii";}
+	else if(d.getDay()==2){document.getElementById('t3').className="hii";}
+	else if(d.getDay()==3){document.getElementById("t4").className="hii";}
+	else if(d.getDay()==4){document.getElementById("t5").className="hii";}
+	else if(d.getDay()==5){document.getElementById("t6").className="hii";}
+	else if(d.getDay()==6){document.getElementById("t7").className="hii";}
 }
